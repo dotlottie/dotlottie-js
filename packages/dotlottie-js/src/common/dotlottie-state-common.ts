0c3c8a5debd4e1126e1156ab27cfb92f7c2a6853
samuelOsborne <samu.osborne@gmail.com>
2023-07-24 19:02:51 +0200

    feat: first draft of exporting states

---
+++ packages/dotlottie-js/src/common/dotlottie-state-common.ts
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2023 Design Barn Inc.
+ */
+
+import type { ZipOptions } from 'fflate';
+
+import type { DotLottieState } from './dotlottie-state';
+import { createError } from './utils';
+
+export interface StateOptions {
+  id: string;
+  state?: DotLottieState;
+  zipOptions?: ZipOptions;
+}
+
+export class DotLottieStateCommon {
+  protected _id: string = '';
+
+  protected _zipOptions: ZipOptions;
+
+  protected _state?: DotLottieState;
+
+  public constructor(options: StateOptions) {
+    this._requireValidId(options.id);
+
+    this._id = options.id;
+
+    this._zipOptions = options.zipOptions ?? {};
+
+    if (options.state) this._state = options.state;
+  }
+
+  public get zipOptions(): ZipOptions {
+    return this._zipOptions;
+  }
+
+  public set zipOptions(zipOptions: ZipOptions) {
+    this._zipOptions = zipOptions;
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set id(id: string) {
+    this._requireValidId(id);
+
+    this._id = id;
+  }
+
+  public get state(): DotLottieState | undefined {
+    return this._state;
+  }
+
+  public set state(state: DotLottieState) {
+    this._state = state;
+  }
+
+  public toString(): string {
+    return JSON.stringify(this._state);
+  }
+
+  protected _requireValidId(id: string | undefined): void {
+    if (!id) {
+      throw createError('Invalid id.');
+    }
+  }
+}
---
+++ packages/dotlottie-js/src/node/lottie-state.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2023 Design Barn Inc.
+ */
+
+import { DotLottieStateCommon, type StateOptions } from '../common/dotlottie-state-common';
+
+export type { StateOptions };
+
+export class LottieState extends DotLottieStateCommon {
+  public constructor(options: StateOptions) {
+    super(options);
+  }
+}
