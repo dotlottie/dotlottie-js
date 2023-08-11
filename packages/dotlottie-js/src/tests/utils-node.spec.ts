749930ce2b0e8d54738a2c11092522056cfb56ab
samuelOsborne <samu.osborne@gmail.com>
2023-08-11 20:37:29 +0200

    chore: added util tests

--- packages/dotlottie-js/src/tests/utils-node.spec.ts
+++ packages/dotlottie-js/src/tests/utils-node.spec.ts
@@ -21,6 +21,8 @@ import {
   loadFromURL,
   createError,
   isValidURL,
+  getStateMachine,
+  getStateMachines,
 } from '../node';
 
 import dotLottieAnimation from './__fixtures__/simple/animation.lottie';
@@ -30,6 +32,7 @@ import dotLottieManifest from './__fixtures__/simple/animation/manifest.json';
 import dotLottieTheme from './__fixtures__/simple/animation/themes/theme1.lss';
 import dotLottieAnimationWithImages from './__fixtures__/simple/big-merged-dotlottie.lottie';
 import bullAnimation from './__fixtures__/simple/bull.lottie';
+import stateAnimation from './__fixtures__/simple/states.lottie';
 
 describe('createError', () => {
   it('returns an instance of Error with the correct message', () => {
@@ -215,6 +218,37 @@ describe('getTheme', () => {
   });
 });
 
+describe('getStateMachine', () => {
+  it('returns undefined if state machine is not found', async () => {
+    const stateMachine = await getStateMachine(stateAnimation, 'invalid_state');
+
+    expect(stateMachine).toBeUndefined();
+  });
+
+  it('gets state machine by id', async () => {
+    const stateMachine = await getStateMachine(stateAnimation, 'state_segments');
+
+    expect(stateMachine).toEqual(
+      JSON.stringify({
+        descriptor: {
+          id: 'state_segments',
+          initial: 'loopState',
+        },
+        states: {
+          loopState: {
+            animationId: 'segments',
+            statePlaybackSettings: {
+              autoplay: true,
+              loop: true,
+              segments: [70, 500],
+            },
+          },
+        },
+      }),
+    );
+  });
+});
+
 describe('getImages', () => {
   it('returns a map of images', async () => {
     const images = await getImages(dotLottieAnimationWithImages);
@@ -238,6 +272,25 @@ describe('getImages', () => {
     const images = await getImages(dotLottieAnimationWithImages, (file) => file.name.startsWith('images/invalid'));
 
     expect(images).toEqual({});
+  });
+});
+
+describe('getStateMachines', () => {
+  it('returns a map of state machines', async () => {
+    const stateMachines = await getStateMachines(stateAnimation);
+
+    // Not all states are being checked
+    const stateSegments = `{"descriptor":{"id":"state_segments","initial":"loopState"},"states":{"loopState":{"animationId":"segments","statePlaybackSettings":{"autoplay":true,"loop":true,"segments":[70,500]}}}}`;
+    const explodingPigeon = `{"descriptor":{"id":"exploding_pigeon","initial":"running"},"states":{"running":{"animationId":"pigeon","statePlaybackSettings":{"autoplay":true,"loop":true,"direction":1,"segments":"bird"},"onClick":{"state":"exploding"}},"exploding":{"animationId":"pigeon","statePlaybackSettings":{"autoplay":true,"loop":3,"direction":1,"segments":"explosion"},"onComplete":{"state":"feathers"}},"feathers":{"animationId":"pigeon","statePlaybackSettings":{"autoplay":true,"loop":false,"direction":1,"segments":"feathers"},"onComplete":{"state":"running"}}}}`;
+
+    expect(stateMachines['state_segments']).toEqual(stateSegments);
+    expect(stateMachines['exploding_pigeon']).toEqual(explodingPigeon);
+  });
+
+  it('returns a map of themes with filter', async () => {
+    const states = await getThemes(dotLottieAnimation, (file) => file.name.startsWith('states/invalid'));
+
+    expect(states).toEqual({});
   });
 });
 
